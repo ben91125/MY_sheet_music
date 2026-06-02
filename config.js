8ecc97041b27db09008d43b10ce5a751836564a3
chacha <chacha@chacha.com>
2026-06-02 17:23:21 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,24 @@
+// config.js
+// 未來你只需要單獨維護這個檔案，新增、修改琴譜都在這裡搞定！
+
+window.scoreData = [
+    {
+        id: 1,
+        title: "聽見下雨的聲音 (完整測試)",
+        composer: "周杰倫",
+        key: "Eb Major",
+        filePaths: [
+            "scores/愛情怎麼喊停p1.png",
+            "scores/愛情怎麼喊停p2.png",
+            "scores/愛情怎麼喊停p3.png",
+            "scores/愛情怎麼喊停p4.png",
+            "scores/愛情怎麼喊停p5.png",
+            "scores/愛情怎麼喊停p6.png",
+            "scores/愛情怎麼喊停p7.png"
+        ],
+        videos: [
+            { versionTitle: "半音階口琴版", youtubeId: "3tmd-ClpJxA" }
+        ]
+    }
+    // 這裡可以繼續往下加新琴譜...
+];
